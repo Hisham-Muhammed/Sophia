5e700c5445b2ff70cbc776f32aedbb715a10dd54
Hisham-Muhammed <87166264+Hisham-Muhammed@users.noreply.github.com>
2021-09-04 16:32:32 +0530

    👑Sophia 2.3 is here 👑
    👑Sophia 2.3 is here 👑
    
    
    Added pm block
    Added badword block
    Added site to pdf commend
    Added new menu list
    Removed verify tick from Bgms
    Added new some bgms
    Added freefire logo pack
    New style for git commend
    Error fixed
    Low repo size so bot run smoothly❤
    Song error fixed
    removed unwanted plugins
    Thanks for ravindu, raashi and somany person ❤
    👑Sophia 2.3 is here 👑

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -1,5 +1,6 @@
 /* Copyright (C) 2020 Yusuf Usta.
 re-editted by afnanplk&Hisham
+sophia v2.3
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 WhatsAsena - Yusuf Usta
