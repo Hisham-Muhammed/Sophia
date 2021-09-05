3da2f29ccc469dc8b3f3e11c7bc439d7acf46cac
ichu <87166264+Hisham-Muhammed@users.noreply.github.com>
2021-09-05 15:07:28 +0530

    Insta fixed💙

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 /* Copyright (C) 2021 ichu.
 
-Licensed under the  GPL-3.0 License;
+Licensed  under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
 WhatsAsena - Yusuf Usta
