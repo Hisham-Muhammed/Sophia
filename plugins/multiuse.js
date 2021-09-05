256e31da1c112c48940e5da1fdf4fe1b0d2a1389
jr-nihal-kkd <90081657+jr-nihal-kkd@users.noreply.github.com>
2021-09-05 10:30:42 +0530

    ♥️Moyalali entho add akki♥️

--- plugins/multiuse.js
+++ plugins/multiuse.js
@@ -32,11 +32,11 @@ Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: false, desc: IG_DESC}, async 
         const msg = `${type}`
 
 	 if (msg === 'image') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, {
-          caption: "Made By WhatsAsenaPublic"
+          caption: "Made By KANNAPPI"
         })}
 		 	 
 	if (msg === 'video') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {
-          caption: "Made By WhatsAsenaPublic"
+          caption: "Made By KANNAPPI"
         })}
 	
         
@@ -71,7 +71,7 @@ Asena.addCommand({ pattern: 'fb ?(.*)', fromMe: false, desc: FBDESC }, async (me
         const msg = `*${CAPTION}*: ${judul}`
 
         await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {
-          caption: "Made By WhatsAsenaPublic"
+          caption: "Made By KANNAPPI"
         })
       })
       .catch(
